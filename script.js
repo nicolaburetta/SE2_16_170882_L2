ab7741463acc9fa74af54ffe1e98b188eb06ed53
nicolaburetta <buretta.nicola@gmail.com>
2016-11-14 17:46:08 +0100

    Item name already exists function
    This function return "true" if the item name already exists, return
    "false" otherwise.

--- script.js
+++ script.js
@@ -4,6 +4,28 @@ function insertItems() {
     document.getElementById("order-button").style.display = "block";
 }
 
-function insertColumn() {
-	var table = document.getElementById("table");
+function placeOrder() {
+	var itemName = document.getElementById("input-order-name");
+    var itemQuantity = document.getElementById("input-order-quantity");
+    
+    if (isNaN(itemQuantity.value) || (!isNaN(itemQuantity.value) && itemQuantity.value <= 0)) {
+        itemQuantity.select();
+        itemQuantity.focus();
+        alert("Current item-quantity box value: " + itemQuantity.value + "\nReplace it with a positive number!");
+    }
+    
+    var c = itemNameAlreadyExists(itemName.value);
+}
+
+function itemNameAlreadyExists(itemName) {
+    var table = document.getElementById("table");
+    var check = false;
+    var index = -1;
+    
+    while (!check && index < table.rows[0].cells.length) {
+        index++;
+        check = (table.rows[0].cells[index].innerHTML == itemName);
+    }
+    
+    return check;
 }
